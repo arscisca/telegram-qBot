6258cf204d30ffba9038dff57ace403fa7626866
shishka0 <s276032@studenti.polito.it>
2020-04-28 23:13:00 +0200

    Add 'queue', 'append', 'next', 'clear' commands

--- commands.py
+++ commands.py
@@ -1,4 +1,21 @@
 import telegram
+import utils.queue as queue
+
+
+def create_queue(context):
+    context.chat_data['queue'] = queue.Queue()
+
+
+def clear_queue(context):
+    context.chat_data.pop('queue')
+
+
+def has_queue(context):
+    return 'queue' in context.chat_data
+
+
+def send_msg(update, context, msg):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 
 def start(update, context):
@@ -9,17 +26,70 @@ def start(update, context):
     )
 
 
+def stop(update, context):
+    clear_queue(context)
+
+
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    send_msg(update, context, update.message.text)
 
 
 def show_args(update, context):
     answer = 'Your args:\n    ' + '\n    '.join(context.args)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+    send_msg(update, context, answer)
+
+
+def print_queue(update, context):
+    if has_queue(context):
+        send_msg(update, context, context.chat_data['queue'].format())
+    else:
+        send_msg(update, context, 'The queue is empty')
+
+
+def append(update, context):
+    """Append an item in queue"""
+    if not has_queue(context):
+        create_queue(context)
+    item = ' '.join(context.args)
+
+    q = context.chat_data['queue']
+    q.append(item)
+    send_msg(update, context, "{} added to queue in position {}".format(item, q.index(item) + 1))
+
+
+def next(update, context):
+    """Pick next turn"""
+    if not has_queue(context):
+        send_msg(update, context, 'The queue is empty')
+        return
+
+    queue = context.chat_data['queue']
+
+    item, _ = queue.pop()
+    if len(queue) == 0:
+        clear_queue(context)
+
+    attached = ' '.join(context.args)
+    message = "{}: ".format(item)
+    if not attached:
+        attached = "it's your turn!"
+    send_msg(update, context, message + attached)
+
+
+def clear(update, context):
+    clear_queue(context)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Queue cleared')
 
 
 COMMANDS = {
+    # Debug commands
+    # 'echo': echo,
+    # 'showargs': show_args,
+
     'start': start,
-    'echo': echo,
-    'showargs': show_args
+    'stop': stop,
+    'queue': print_queue,
+    'append': append,
+    'next': next,
+    'clear': clear
 }
